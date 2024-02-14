3dc00b8b38af71094337796cca4138596437b53f
Mason DiGiorgio <mason@digiorgiofamily.net>
2024-02-14 18:10:25 -0500

    Fixed some bugs

--- Bolt_Dash/imports/Backend/backend.cpp
+++ Bolt_Dash/imports/Backend/backend.cpp
@@ -3,7 +3,7 @@
 
 // Create Backend class which can be included in QML
 Backend::Backend(QObject *parent) : QObject(parent), m_motorTemp{}, m_auxVoltage{}, m_auxPercent{},
-                                    m_packSOC{}, m_highCellTemp{}, m_lowCellTemp, m_bmsTemp{}, m_motorSpeed{}, m_bikeSpeed{} {
+                                    m_packSOC{}, m_highCellTemp{}, m_lowCellTemp{}, m_bmsTemp{}, m_motorSpeed{}, m_bikeSpeed{} {
     std::thread update_vars(&Backend::updateVars, this);
     update_vars.detach();
 }
@@ -27,108 +27,108 @@ void Backend::updateVars() {
 }
 
 // Functions to get variable values
-{ // Brackets for collapsability
-    double Backend::motorTemp() const {
-        return m_motorTemp;
-    }
-
-    double Backend::auxVoltage() const {
-        return m_auxVoltage;
-    }
-
-    double Backend::auxPercent() const {
-        return m_auxPercent;
-    }
-
-    double Backend::packSOC() const {
-        return m_packSOC;
-    }
-
-    double Backend::highCellTemp() const {
-        return m_highCellTemp;
-    }
-
-    double Backend::lowCellTemp() const {
-        return m_lowCellTemp;
-    }
-
-    double Backend::bmsTemp() const {
-        return m_bmsTemp;
-    }
-
-    double Backend::motorSpeed() const {
-        return m_motorSpeed;
-    }
-
-    double Backend::bikeSpeed() const {
-        return m_bikeSpeed;
-    }
+// { // Brackets for collapsability
+double Backend::motorTemp() const {
+    return m_motorTemp;
 }
+
+double Backend::auxVoltage() const {
+    return m_auxVoltage;
+}
+
+double Backend::auxPercent() const {
+    return m_auxPercent;
+}
+
+double Backend::packSOC() const {
+    return m_packSOC;
+}
+
+double Backend::highCellTemp() const {
+    return m_highCellTemp;
+}
+
+double Backend::lowCellTemp() const {
+    return m_lowCellTemp;
+}
+
+double Backend::bmsTemp() const {
+    return m_bmsTemp;
+}
+
+double Backend::motorSpeed() const {
+    return m_motorSpeed;
+}
+
+double Backend::bikeSpeed() const {
+    return m_bikeSpeed;
+}
+// }
 
 // Setter Functions
-{ // Brackets for collapsability
-    void Backend::setMotorTemp(const double temp) {
-        if (m_motorTemp != temp) {
-            m_motorTemp = temp;
-            emit motorTempChanged();
-        }
-    }
-
-    void Backend::setAuxVoltage(const double cap) {
-        if (m_auxVoltage != cap) {
-            m_auxVoltage = cap;
-            emit auxVoltageChanged();
-        }
-    }
-
-    void Backend::setAuxPercent(const double cap) {
-        if (m_auxPercent != cap) {
-            m_auxPercent = cap;
-            emit auxPercentChanged();
-        }
-    }
-
-    void Backend::setPackSOC(const double soc) {
-        if (m_packSOC != soc) {
-            m_packSOC = soc;
-            emit packSOCChanged();
-        }
-    }
-
-    void Backend::setHighCellTemp(const double temp) {
-        if (m_highCellTemp != soc) {
-            m_highCellTemp = soc;
-            emit highCellTempChanged();
-        }
-    }
-
-    void Backend::setLowCellTemp(const double temp) {
-        if (m_lowCellTemp != soc) {
-            m_lowCellTemp = soc;
-            emit lowCellTempChanged();
-        }
-    }
-
-    void Backend::setBmsTemp(const double temp) {
-        if (m_bmsTemp != soc) {
-            m_bmsTemp = soc;
-            emit bmsTempChanged();
-        }
-    }
-
-    void Backend::setMotorSpeed(const double speed) {
-        if (m_motorSpeed != soc) {
-            m_motorSpeed = soc;
-            emit motorSpeedChanged();
-        }
-    }
-
-    void Backend::setBikeSpeed(const double speed) {
-        if (m_bikeSpeed != soc) {
-            m_bikeSpeed = soc;
-            emit bikeSpeedChanged();
-        }
+// { // Brackets for collapsability
+void Backend::setMotorTemp(const double temp) {
+    if (m_motorTemp != temp) {
+        m_motorTemp = temp;
+        emit motorTempChanged();
     }
 }
+
+void Backend::setAuxVoltage(const double cap) {
+    if (m_auxVoltage != cap) {
+        m_auxVoltage = cap;
+        emit auxVoltageChanged();
+    }
+}
+
+void Backend::setAuxPercent(const double cap) {
+    if (m_auxPercent != cap) {
+        m_auxPercent = cap;
+        emit auxPercentChanged();
+    }
+}
+
+void Backend::setPackSOC(const double soc) {
+    if (m_packSOC != soc) {
+        m_packSOC = soc;
+        emit packSOCChanged();
+    }
+}
+
+void Backend::setHighCellTemp(const double temp) {
+    if (m_highCellTemp != temp) {
+        m_highCellTemp = temp;
+        emit highCellTempChanged();
+    }
+}
+
+void Backend::setLowCellTemp(const double temp) {
+    if (m_lowCellTemp != temp) {
+        m_lowCellTemp = temp;
+        emit lowCellTempChanged();
+    }
+}
+
+void Backend::setBmsTemp(const double temp) {
+    if (m_bmsTemp != temp) {
+        m_bmsTemp = temp;
+        emit bmsTempChanged();
+    }
+}
+
+void Backend::setMotorSpeed(const double speed) {
+    if (m_motorSpeed != speed) {
+        m_motorSpeed = speed;
+        emit motorSpeedChanged();
+    }
+}
+
+void Backend::setBikeSpeed(const double speed) {
+    if (m_bikeSpeed != speed) {
+        m_bikeSpeed = speed;
+        emit bikeSpeedChanged();
+    }
+}
+// }
 
 std::thread can_thread(can::run);
