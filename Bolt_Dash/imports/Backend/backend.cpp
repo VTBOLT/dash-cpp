eae53d54bd7d7f701d62426b9fd71922678cb999
Mason DiGiorgio <mason@digiorgiofamily.net>
2024-02-14 18:20:35 -0500

    Add debug message for what the data is being set to

--- Bolt_Dash/imports/Backend/backend.cpp
+++ Bolt_Dash/imports/Backend/backend.cpp
@@ -22,6 +22,8 @@ void Backend::updateVars() {
         setMotorSpeed(data.motor_speed);                // rpm
         setBikeSpeed(data.bike_speed);                  // mph
         m.unlock();
+        // Debug Message
+        std::cout << "MotorTemp: " << motorTemp() << " AuxVoltage: " << auxVoltage() << " AuxPercent: " << auxPercent() << " PackSOC: " << packSOC() << " HighCellTemp: " << highCellTemp() << " LowCellTemp: " << lowCellTemp() << " BmsTemp: " << bmsTemp() << " MotorSpeed: " << motorSpeed() << " BikeSpeed: " << bikeSpeed() << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
